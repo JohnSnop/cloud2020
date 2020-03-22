7decbe19a804262273fe5f444118b43d287617cc
wf <285682283@qq.com>
2020-03-22 19:06:00 +0800

    springcloud2020-消息总线-Springcloud bus

---
+++ cloud-config-client3366/src/main/java/com/wf/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.wf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wf
+ * @create 2020-03-22 17:13
+ * @desc
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client3366/src/main/java/com/wf/springcloud/controller/ConfigController.java
@@ -0,0 +1,29 @@
+package com.wf.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wf
+ * @create 2020-03-22 17:15
+ * @desc
+ **/
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort：" + serverPort + "\t" + configInfo;
+    }
+}
